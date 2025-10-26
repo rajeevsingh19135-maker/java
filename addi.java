c68e755f3651296db44caff28b12a382a5a5de5a
Rajeev <rajeevsingh19135>
2025-10-27 02:21:09 +0530

    add new feature

--- addi.java
+++ addi.java
@@ -1,8 +1,6 @@
 public class addi {
     public static void main(String[] args) {
-        System.out.println("hiiii");
         System.out.println("Rajeev");
-        System.out.println("Singh");
         System.out.println(21_22);
     }
 }
