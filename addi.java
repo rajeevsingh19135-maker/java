0fbfb86cd15ac75795c56da0549d741404198d7d
Rajeev <rajeevsingh19135>
2025-10-27 02:17:59 +0530

    add in main

--- addi.java
+++ addi.java
@@ -2,7 +2,5 @@ public class addi {
     public static void main(String[] args) {
         System.out.println("hiiii");
         System.out.println("Rajeev");
-        System.out.println("Singh");
-        System.out.println(21_22);
     }
 }
